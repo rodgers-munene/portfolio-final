bf111b34cc5eb7e3d82fa04b711b04f18f6eb775
rodgers-munene <munenerodgers72@gmail.com>
2025-06-14 21:15:18 +0300

    section: projects section in home page

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,12 +1,14 @@
 import React from 'react'
 import Hero from '../components/Hero'
 import FunnyQuote from '../components/FunnyQuote'
+import Projects from '../components/Projects'
 
 const Home = () => {
   return (
     <div className='min-h-screen'>
         <Hero />
         <FunnyQuote />
+        <Projects />
     </div>
   )
 }
---
+++ src/services/Data.js
@@ -0,0 +1,31 @@
+import SwiftCart from '../assets/swiftCart.png'
+import Event from '../assets/EventSystem.png'
+import FilmSage from '../assets/FilmSage.png'
+
+
+export const ProjectsData = [
+    {
+        title: "SwiftCart",
+        description: "A modern, full-stack e-commerce platform built using Node.js, React, and MongoDB, designed to deliver a fast, user-friendly shopping experience with features like M-Pesa integration.",
+        shortDesc: "E-commerce platform",
+        image: SwiftCart,
+        stack: ["NodeJs", "ReactJs", "ExpressJs", "Tailwind"]
+
+    },
+    {
+        title: "Event Management System",
+        description: "A modern, responsive Event Management System built with React and Vite.",
+        shortDesc: "Manage Your Events",
+        image: Event,
+        stack: ["NodeJs", "ReactJs", "ExpressJs"]
+
+    },
+    {
+        title: "FilmSage",
+        description: "an intelligent movie recommendation system designed to help users discover their next favorite movie.",
+        shortDesc: "Movie Recommendation System",
+        image: FilmSage,
+        stack: ["Framer", "ReactJs", "TypeScript", "Tailwind"]
+
+    }
+]
